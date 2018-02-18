892dfed0704eb5b15eee4da5f4de68a17ade09fd
hanx940@ACCOUNTS.AD.WUSTL.EDU <hanx940@ACCOUNTS.AD.WUSTL.EDU@fa64eb7a-3e15-496f-a70f-eb50fc3332bc>
2018-02-18 01:57:02 +0000

    hw4 problem 2 submission

--- cse427s_sp18/hw4/SentimentPartitionTest.java
+++ cse427s_sp18/hw4/SentimentPartitionTest.java
@@ -14,25 +14,36 @@ public class SentimentPartitionTest {
 	@Test
 	public void testSentimentPartition() {
 
-		spart=new SentimentPartitioner<Text, IntWritable>();
-		spart.setConf(new Configuration());
-		int result;		
+		spart=new SentimentPartitioner();  // remove type argument to clear error 
+		spart.setConf(new Configuration()); //read new configuration and assign to spart
+		// now set up three more result in type integer
+		int result;
+		int result1;
+		int result2;
+		int result3;
 		
 		/*
-		 * A test for word "beauty" with expected outcome 0 would   
-		 * look like this:
+		 * A test for word "beauty" with expected outcome 0  
 		 */
 		result = spart.getPartition(new Text("beauty"), new IntWritable(23), 3);
 		assertEquals(result,0);	
-
 		/*
-		 * Test the words "love", "deadly", and "zodiac". 
-		 * The expected outcomes should be 0, 1, and 2. 
+		 * Test the words "love", expected result is 0 
 		 */
+		result1 = spart.getPartition(new Text("love"), new IntWritable(23), 3);
+		assertEquals(result1,0);	
         
  		/*
-		 * TODO implement
+		 * Test the word "deadly", expected result is 1
 		 */          
+		result2 = spart.getPartition(new Text("deadly"), new IntWritable(23), 3);
+		assertEquals(result2,1);	
+		/*
+		 * Test the word "zodiac", expected result is 2
+		 */
+		result3 = spart.getPartition(new Text("zodiac"), new IntWritable(23), 3);
+		assertEquals(result3,2);	
+		// end of the test
 		
 	}
 
